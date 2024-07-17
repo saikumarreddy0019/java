35ec00bda98c608c78d30dfc61884090a47b17b5
unknown <you@example.com>
2024-07-17 18:52:58 +0530

    some files

---
+++ jaggu.java
@@ -0,0 +1,10 @@
+public class sai{
+    public static void main(String[] args){
+        int a =40
+        int b =40
+        int c= 80
+        System.out.println(c+b)
+        System.out.println(a+b)
+        System.out.println(a-b-c)
+    }
+}
